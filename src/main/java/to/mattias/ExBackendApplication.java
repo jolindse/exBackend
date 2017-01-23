068573097b894eb9c825dbc163250c559a782b4f
Mattias Larsson <mattias.larsson75@outlook.com>
2017-01-23 08:08:57 +0100

    initial commit

---
+++ src/main/java/to/mattias/ExBackendApplication.java
@@ -0,0 +1,12 @@
+package to.mattias;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExBackendApplication.class, args);
+	}
+}
