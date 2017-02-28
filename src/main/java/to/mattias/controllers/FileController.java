c65cd4ca202c407906de8e49646871d9e752b4a1
Mattias Larsson <mattias.larsson75@outlook.com>
2017-03-01 00:43:31 +0100

    Some minor refactoring

--- src/main/java/to/mattias/controllers/FileController.java
+++ src/main/java/to/mattias/controllers/FileController.java
@@ -1,7 +1,8 @@
 package to.mattias.controllers;
 
-import org.apache.tomcat.util.http.fileupload.FileUploadBase;
+import org.apache.tomcat.util.http.fileupload.IOUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -10,26 +11,50 @@ import to.mattias.entities.NoteObj;
 import to.mattias.services.FileService;
 
 import javax.servlet.http.HttpServletResponse;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
 
 /**
  * <h1>Created by Mattias on 2017-02-18.</h1>
  */
 @RestController
 @CrossOrigin("*")
-@RequestMapping("/file")
 public class FileController {
 
     @Autowired
     private FileService fileService;
 
-    @PostMapping
+    @Autowired
+    private Environment env;
+
+    @RequestMapping(value = "/file", method = RequestMethod.POST)
     public ResponseEntity<NoteObj> uploadFile(@RequestParam("file") MultipartFile file) throws IOException {
         NoteObj returnObj = fileService.store(file);
         if (returnObj != null) {
             return new ResponseEntity<>(returnObj, HttpStatus.OK);
         } else {
-            return new ResponseEntity<>(new NoteObj(), HttpStatus.INTERNAL_SERVER_ERROR);
+            // TODO: Log the error
+            return new ResponseEntity<>(new NoteObj(), HttpStatus.NO_CONTENT);
+        }
+    }
+
+    @RequestMapping(value = "/assets/{dirName}/{fileName:.+}", method = RequestMethod.GET)
+    public void serveFile(@PathVariable("dirName") String dirName, @PathVariable("fileName") String fileName, HttpServletResponse response) {
+        String baseDir = env.getProperty("upload.base.dir");
+        try {
+            InputStream is = new FileInputStream(baseDir + dirName + "\\" + fileName);
+            IOUtils.copy(is, response.getOutputStream());
+            response.flushBuffer();
+        } catch (FileNotFoundException e) {
+            // TODO: Log the exception
+            e.printStackTrace();
+            response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+        } catch (IOException e) {
+            // TODO: Log the exception
+            e.printStackTrace();
+            response.setStatus(HttpServletResponse.SC_NO_CONTENT);
         }
     }
 }
