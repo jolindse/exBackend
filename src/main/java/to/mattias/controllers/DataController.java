58d8c7fffcfdcd2ffa9ad76b65e04ab59af1a575
Mattias Larsson <mattias@mattias.to>
2017-05-28 16:43:57 +0200

    Creating more data with /data/initdata

--- src/main/java/to/mattias/controllers/DataController.java
+++ src/main/java/to/mattias/controllers/DataController.java
@@ -1,9 +1,12 @@
 package to.mattias.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import to.mattias.constants.TaskStatus;
 import to.mattias.constants.UserRole;
 import to.mattias.entities.Project;
 import to.mattias.entities.Sprint;
@@ -32,6 +35,7 @@ public class DataController {
 
 
     @GetMapping(value="/initdata")
+    @ResponseStatus(HttpStatus.CREATED)
     public String initData() {
 
         User user1 = new User();
@@ -55,37 +59,14 @@ public class DataController {
         user3.setPassword("password");
         user3.setUsername("user3");
 
-        Task task1 = new Task();
-        Task task2 = new Task();
-        Task task3 = new Task();
-        Task task4 = new Task();
+        Project project1 = createProject1();
+        Project project2 = createProject2();
 
-        task1.setTaskTitle("Task 1");
-        task2.setTaskTitle("Task 2");
-        task3.setTaskTitle("Task 3");
-        task4.setTaskTitle("Task 4");
+        user2.setProjectRole(project1.getId(),UserRole.USER);
+        user3.setProjectRole(project1.getId(),UserRole.CUSTOMER);
 
-        Sprint sprint1 = new Sprint();
-        sprint1.addTask(task1);
-        sprint1.setSprintTitle("Sprint 1");
-
-        Project proj1 = new Project();
-        proj1.setProjectTitle("Proj 1");
-        Project proj2 = new Project();
-        proj2.setProjectTitle("Proj 2");
-
-        proj1.addSprint(sprint1);
-
-        Project project1 = projRepo.save(proj1);
-        Project project2 = projRepo.save(proj2);
-
-        taskRepo.save(task1);
-        taskRepo.save(task2);
-        taskRepo.save(task3);
-        taskRepo.save(task4);
-
-        user2.setProjectRole(project2.getId(),UserRole.USER);
-        user3.setProjectRole(project2.getId(),UserRole.CUSTOMER);
+        user2.setProjectRole(project2.getId(), UserRole.CUSTOMER);
+        user3.setProjectRole(project2.getId(), UserRole.USER);
 
         userRepo.save(user1);
         userRepo.save(user2);
@@ -94,5 +75,137 @@ public class DataController {
         return "ok";
     }
 
+    private Project createProject1() {
+        Project project = new Project();
+        project.setProjectTitle("Project 1");
+        project.setProjectDescription("Description for Project 1");
 
+        Sprint sprint1 = new Sprint();
+        project.addSprint(sprint1);
+
+        Task task1 = new Task();
+        task1.setTaskTitle("Task 1");
+        task1.setTaskDescription("Description for Task 1");
+        task1.setTaskStatus(TaskStatus.TODO);
+        sprint1.addTask(task1);
+        taskRepo.save(task1);
+
+        Task task2 = new Task();
+        task2.setTaskTitle("Task 2");
+        task2.setTaskDescription("Description for Task 2");
+        task2.setTaskStatus(TaskStatus.WIP);
+        sprint1.addTask(task2);
+        taskRepo.save(task2);
+
+        Task task3 = new Task();
+        task3.setTaskTitle("Task 3");
+        task3.setTaskDescription("Description for Task 3");
+        task3.setTaskStatus(TaskStatus.TEST);
+        sprint1.addTask(task3);
+        taskRepo.save(task3);
+
+        Task task4 = new Task();
+        task4.setTaskTitle("Task 4");
+        task4.setTaskDescription("Description for Task 4");
+        task4.setTaskStatus(TaskStatus.DONE);
+        sprint1.addTask(task4);
+        taskRepo.save(task4);
+
+        Task task5 = new Task();
+        task5.setTaskTitle("Task 5");
+        task5.setTaskDescription("Description for Task 5");
+        task5.setTaskStatus(TaskStatus.TEST);
+        sprint1.addTask(task5);
+        taskRepo.save(task5);
+
+        Task task6 = new Task();
+        task6.setTaskTitle("Task 6");
+        task6.setTaskDescription("Description for Task 6");
+        task6.setTaskStatus(TaskStatus.WIP);
+        sprint1.addTask(task6);
+        taskRepo.save(task6);
+
+        Task task7 = new Task();
+        task7.setTaskTitle("Task 7");
+        task7.setTaskDescription("Description for Task 7");
+        task7.setTaskStatus(TaskStatus.TODO);
+        sprint1.addTask(task7);
+        taskRepo.save(task7);
+
+        Task task8 = new Task();
+        task8.setTaskTitle("Task 8");
+        task8.setTaskDescription("Description for Task 8");
+        task8.setTaskStatus(TaskStatus.WIP);
+        sprint1.addTask(task8);
+        taskRepo.save(task8);
+
+        return projRepo.save(project);
+    }
+
+    private Project createProject2() {
+        Project project = new Project();
+        project.setProjectTitle("Project 2");
+        project.setProjectDescription("Description for Project 2");
+
+        Sprint sprint2 = new Sprint();
+        project.addSprint(sprint2);
+
+        Task task9 = new Task();
+        task9.setTaskTitle("Task 9");
+        task9.setTaskDescription("Description for Task 9");
+        task9.setTaskStatus(TaskStatus.TODO);
+        sprint2.addTask(task9);
+        taskRepo.save(task9);
+
+        Task task10 = new Task();
+        task10.setTaskTitle("Task 10");
+        task10.setTaskDescription("Description for Task 10");
+        task10.setTaskStatus(TaskStatus.WIP);
+        sprint2.addTask(task10);
+        taskRepo.save(task10);
+
+        Task task11 = new Task();
+        task11.setTaskTitle("Task 11");
+        task11.setTaskDescription("Description for Task 11");
+        task11.setTaskStatus(TaskStatus.TEST);
+        sprint2.addTask(task11);
+        taskRepo.save(task11);
+
+        Task task12 = new Task();
+        task12.setTaskTitle("Task 12");
+        task12.setTaskDescription("Description for Task 12");
+        task12.setTaskStatus(TaskStatus.DONE);
+        sprint2.addTask(task12);
+        taskRepo.save(task12);
+
+        Task task13 = new Task();
+        task13.setTaskTitle("Task 13");
+        task13.setTaskDescription("Description for Task 13");
+        task13.setTaskStatus(TaskStatus.TEST);
+        sprint2.addTask(task13);
+        taskRepo.save(task13);
+
+        Task task14 = new Task();
+        task14.setTaskTitle("Task 14");
+        task14.setTaskDescription("Description for Task 14");
+        task14.setTaskStatus(TaskStatus.WIP);
+        sprint2.addTask(task14);
+        taskRepo.save(task14);
+
+        Task task15 = new Task();
+        task15.setTaskTitle("Task 15");
+        task15.setTaskDescription("Description for Task 15");
+        task15.setTaskStatus(TaskStatus.TODO);
+        sprint2.addTask(task15);
+        taskRepo.save(task15);
+
+        Task task16 = new Task();
+        task16.setTaskTitle("Task 16");
+        task16.setTaskDescription("Description for Task 16");
+        task16.setTaskStatus(TaskStatus.WIP);
+        sprint2.addTask(task16);
+        taskRepo.save(task16);
+
+        return projRepo.save(project);
+    }
 }
